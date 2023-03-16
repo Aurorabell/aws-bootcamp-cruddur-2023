cbff0f307c0ccef462c375465f4c62043b1751d3
Christabel Aurora Khumalo <khumalochristabelle@gmail.com>
2023-03-16 11:49:46 +0000

    implementing CloudWatch logs

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -28,6 +28,22 @@ from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProces
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+#AWSWatchTower
+import watchtower
+import logging
+from time import strftime
+
+# Configuring Logger to Use CloudWatch >>> Remove the hashtags to turn on CLoudWatch
+#LOGGER = logging.getLogger(__name__)
+#LOGGER.setLevel(logging.DEBUG)
+#console_handler = logging.StreamHandler()
+#cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+#LOGGER.addHandler(console_handler)
+#LOGGER.addHandler(cw_handler)
+#LOGGER.info("some message")
+#LOGGER.info("test log")
+
+
 #Honeycomb: Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
@@ -62,6 +78,12 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -5,6 +5,7 @@ tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run():
+    #logger.info("HomeActivities")
 #Creating Spans
     with tracer.start_as_current_span("home.activities-mock-data"):
         span = trace.get_current_span()
