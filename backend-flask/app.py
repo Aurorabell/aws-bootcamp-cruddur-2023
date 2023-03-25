0b8a9d9eaff681f81ea2512f52682e86faa530fe
Christabel Aurora Khumalo <khumalochristabelle@gmail.com>
2023-03-25 11:08:22 +0000

    check jwt again

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -97,8 +97,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -151,6 +151,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
+  print()
   data = HomeActivities.run()
   return data, 200
 
