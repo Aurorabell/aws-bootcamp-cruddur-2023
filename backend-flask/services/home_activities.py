f746de1d72680454b5797d2e851a62de5a2375cb
Christabel Aurora Khumalo <38355246+Aurorabell@users.noreply.github.com>
2023-04-07 20:34:01 +0400

    updating

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 #To create spans, you need to get a Tracer
 from opentelemetry import trace
-from lib.db import pool, query_wrap_object, query_wrap_array
+from lib.db import pool #query_wrap_object, query_wrap_array
 
 #tracer = trace.get_tracer("home.activities")
 
@@ -14,15 +14,18 @@ class HomeActivities:
         #now = datetime.now(timezone.utc).astimezone()
         #span.set_attribute("app.now", now.isoformat())
         
-        sql = query_wrap_array("""
-        SELECT * FROM activities          
-        """)
+       # sql = query_wrap_array("""
+       #SELECT * FROM activities          
+       # """)
         #print (sql)
-        with pool.connection() as conn:
-          with conn.cursor() as cur:
-            cur.execute(sql)
-            # this will return a tuple
-            # the first field being the data
-            json = cur.fetchone()
-        return json[0]
-        return results
+       # with pool.connection() as conn:
+       #  with conn.cursor() as cur:
+       #     cur.execute(sql)
+       #      this will return a tuple
+       #     the first field being the data
+       #    json = cur.fetchone()
+        #return json[0]
+         #return results
+      sql = db.template('activities','home')
+    results = db.query_array_json(sql)
+    return results
